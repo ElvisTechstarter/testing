b1ce8fd45d5651da508679e95e98da2023cc8c97
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-03-04 23:22:12 +0100

    Aufgabe 2.2

---
+++ minMax.test.js
@@ -0,0 +1,37 @@
+const minMax = require("./minMax");
+
+test("2 ist kleiner als 5 ", () => {
+  const result = minMax.min(2, 5);
+
+  expect(result).toBe(2);
+});
+
+test("-5 ist kleiner als 2", () => {
+  const result = minMax.min(-5, 2);
+
+  expect(result).toBe(-5);
+});
+
+test("0 ist kleiner als 5", () => {
+  const result = minMax.min(0, 5);
+
+  expect(result).toBe(0);
+});
+
+test("5 ist größer als 2", () => {
+  const result = minMax.max(5, 2);
+
+  expect(result).toBe(5);
+});
+
+test("2 ist größer als -5", () => {
+  const result = minMax.max(2, -5);
+
+  expect(result).toBe(2);
+});
+
+test("5 ist größer als 0", () => {
+  const result = minMax.max(5, 0);
+
+  expect(result).toBe(5);
+});
