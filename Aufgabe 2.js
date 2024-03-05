fdeec6f3c32bf481450dbb275941de82a4311716
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-03-05 18:53:58 +0100

    Aufgabe 2

---
+++ Aufgabe 2.js
@@ -0,0 +1,19 @@
+- Wenn zwei Tests ".todo"-Suffix beinhalten
+    
+    - 3
+
+- Wenn ein Test ".only"-Suffix beinhaltet
+    
+    - 1
+
+- Wenn drei Tests ".skip"-Suffix beinhalten
+   
+    - 2
+
+- Wenn 2 Tests in einem Describe geschachtelt sind und dieses Describe den Suffix ".only" trägt
+    
+    - 2
+
+- Wenn 2 Tests den Suffix ".failing" beinhalten
+
+    - 5
